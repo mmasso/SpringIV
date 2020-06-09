ea7fd62cd69d0c3f39a59838fadd18b64559649c
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 14:05:49 +0200

    feat: test multiples alumnes ok

--- src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -1,21 +1,41 @@
 package org.formacio.setmana2.servei;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
+import org.formacio.setmana2.repositori.RepositoriEscola;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServeiEscola {
 
-	
 	/**
 	 * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
 	 * Per tant, els hem de carregar, no crear de nou.
 	 * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
 	 */
+	
+	@Autowired
+	RepositoriEscola repositori;
+	
+	@Transactional
 	public List<Matricula> apunta (String curs, List<String> alumnes) throws EdatIncorrecteException {
-		return null;
+		
+		List<Matricula> listaMatriculas = new ArrayList<Matricula>();
+		
+		for(String alumne : alumnes) {
+		
+			Matricula matricula = repositori.apunta(alumne, curs);
+		
+			listaMatriculas.add(matricula);
+		
+		}
+	
+		return listaMatriculas;
     }
 }
