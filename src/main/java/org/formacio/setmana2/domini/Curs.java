5069cb33b950853a2fea7f77bfb814fd9f4e35f9
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 13:04:47 +0200

    feat: pasamos segundo caso test

--- src/main/java/org/formacio/setmana2/domini/Curs.java
+++ src/main/java/org/formacio/setmana2/domini/Curs.java
@@ -1,9 +1,21 @@
 package org.formacio.setmana2.domini;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_cursos")
 public class Curs {
 
+	//create table t_cursos (cur_nom varchar(255) not null, cur_edatminima integer, primary key (cur_nom))
+	
+	@Id
+	@Column(name="cur_nom")
 	private String nom;
     
+	@Column(name="cur_edatminima")
 	private int edatMinima;
 	
 	public String getNom() {
