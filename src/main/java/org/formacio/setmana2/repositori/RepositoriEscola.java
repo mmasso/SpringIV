5f1b376e72abf0d068f5b5526886219ae47baba0
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 13:26:34 +0200

    feat repositori Escola: test_carregacurs

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -1,17 +1,29 @@
 package org.formacio.setmana2.repositori;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+
+@Repository
+@Transactional
 public class RepositoriEscola {
 
+	@PersistenceContext
+	private EntityManager em;
 	
 	public Curs carregaCurs(String nom) {
-		return null;
+		Curs curs = em.find(Curs.class, nom);
+		return curs;
 	}
 	
 	
